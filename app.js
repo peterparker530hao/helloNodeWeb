07c2f078690ef4ea179f514226bf0e39cf9d4da3
peterparker530hao <tangjia_email@163.com>
2019-04-26 08:55:51 +0800

    通信入口js文件

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -14,26 +14,37 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
+//登录拦截器
+app.all('/*', function (req, res, next) {
+    var url = req.url.split('?')[0];
+    if (url == '/' ) {
+        res.render('index');
+    }else{
+        next();
+    }
+});
+
+
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    res.render('404');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-  });
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.status =err.status || 500;
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 module.exports = app;
