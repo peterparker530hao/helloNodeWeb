9b5c825dfad065acf501cd6f4c94d886864376bc
peterparker530hao <tangjia_email@163.com>
2019-04-26 08:55:14 +0800

    操作数据库的js文件

--- utility/databaseDriver.js
+++ utility/databaseDriver.js
@@ -10,34 +10,36 @@ var pool = mysql.createPool({
 });
 console.log('开启数据库连接池！');
 
-connect = function (callback) {
-    console.log('获取数据库连接之前有' + pool._allConnections.length + '条连接！');
+
+dao.execute = function (sql, callback) {
+    console.log('sql为<'+sql+'>，在获取数据库连接之前有' + pool._allConnections.length + '条连接！');
     pool.getConnection(function (err, connection) {
         if (err) {//如果有错就打印并返回
             console.log('获取数据库连接失败！' + err);
-            return;
         } else {
             console.log('获取数据库连接成功！' + '当前有' + pool._allConnections.length + '条连接！');
-            callback(connection);
+            connection.query(sql, function (qerr, vals, fields) {
+                if (qerr) {//执行失败
+                    console.log('执行失败:', qerr.message);
+                    callback(qerr);
+                } else {//执行成功
+                    console.log('执行成功！');
+                    callback(qerr, vals, fields);//事件驱动回调
+                }
+                repay(connection);//释放连接
+            });
+
         }
     });
 };
 
-
-dao.execute = function (sql, callback) {
-    connect(function (connection) {
-        connection.query(sql, function (qerr, vals, fields) {
-            if (qerr) {//执行失败
-                console.log('执行失败:', qerr.message);
-                callback(qerr);
-            } else {//执行成功
-                console.log('执行成功！');
-                callback(qerr, vals, fields);//事件驱动回调
-            }
-            repay(connection);//释放连接
-        });
-    });
+dao.escape = function (identifier) {
+    return pool.escape(identifier);
 };
+dao.connection = function (identifier) {
+    return pool.escapeId(identifier);
+};
+
 
 repay = function (connection) {
     pool.releaseConnection(connection);
